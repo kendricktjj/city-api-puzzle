6706cd135f6005c649fdd0dcae69999c8e451f1c
Jian Jing Tan <kendricktjj@gmail.com>
2023-06-11 16:31:12 +0800

    Add Product List table to Index and change CallAPI() implementation to old demo style

--- CityFacilitiesAPIPuzzle/Controllers/HomeController.cs
+++ CityFacilitiesAPIPuzzle/Controllers/HomeController.cs
@@ -7,12 +7,28 @@ using System.Net.Http;
 using CityFacilitiesAPIPuzzle.Models;
 using Newtonsoft.Json;
 using CityFacilitiesAPIPuzzle.Helpers;
+using System.Threading.Tasks;
 
 namespace CityFacilitiesAPIPuzzle.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            /*
+             * Start the program with a blank list of products.
+             * Add a single dummy product to help with refreshing the table after data is loaded.
+             * There's probably a better way to do this, but I don't have the time to look into it.
+             */
+            Product dummy = new Product();
+            var initialList = new List<Product>();
+            initialList.Add(dummy);
+
+            return View(initialList);
+        }
+
+        [HttpPost]
+        public ActionResult GetProducts()
         {
             APIInterface<Product> apiI = new APIInterface<Product>();
             string apiURL = "https://alltheclouds.com.au/api/Products";
@@ -22,21 +38,7 @@ namespace CityFacilitiesAPIPuzzle.Controllers
 
             var productList = (List<Product>) apiI.CallAPI(apiURL, headers);
 
-            return View(productList);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return Json(productList);
         }
     }
 }
--- CityFacilitiesAPIPuzzle/Helpers/APIInterface.cs
+++ CityFacilitiesAPIPuzzle/Helpers/APIInterface.cs
@@ -6,49 +6,54 @@ using System.Net.Http;
 using System.Web;
 using CityFacilitiesAPIPuzzle.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace CityFacilitiesAPIPuzzle.Helpers
 {
     public class APIInterface<T>
     {
-        private static HttpClientHandler handler = new HttpClientHandler();
-        private static HttpClient client = new HttpClient(handler);
-
         /// <summary>
         /// Method that submits a request to the given API URL.
         /// </summary>
         /// <param name="apiURL"> URL of API to be queried. </param>
         /// <param name="headers"> Optional parameter containing KeyValuePairs of headers and header values to be added to API call. </param>
-        /// <returns> Returns a list of objects. Returns an empty list if the API call is unsuccessful.</returns>
+        /// <returns> Returns a list of objects from the API response. Returns an empty list if the API call is unsuccessful.</returns>
         public IEnumerable<T> CallAPI(string apiURL, IDictionary<string, string> headers = null)
         {
-            handler.UseDefaultCredentials = true;
-            var uri = new Uri(apiURL);
-
-            // Add headers to API request if any are provided
-            if(headers != null)
+            using (var handler = new HttpClientHandler())
             {
-                foreach (KeyValuePair<string, string> keyValuePair in headers)
+                handler.UseDefaultCredentials = true;
+
+                using (var client = new HttpClient(handler))
                 {
-                    client.DefaultRequestHeaders.Add(keyValuePair.Key, keyValuePair.Value);
+                    var uri = new Uri(apiURL);
+
+                    // Add headers to API request if any are provided
+                    if (headers != null)
+                    {
+                        foreach (KeyValuePair<string, string> keyValuePair in headers)
+                        {
+                            client.DefaultRequestHeaders.Add(keyValuePair.Key, keyValuePair.Value);
+                        }
+                    }
+                    var response = client.GetAsync(uri); // Should move this to Task to be proper async
+                                                         //response.Result.EnsureSuccessStatusCode();
+
+                    if (!response.Result.IsSuccessStatusCode)
+                    {
+                        System.Diagnostics.Debug.WriteLine("API call failed.");
+                        System.Diagnostics.Debug.WriteLine($"Error: {response.Result.Content.ReadAsStringAsync().Result}");
+
+                        return new List<T>();
+                    }
+                    System.Diagnostics.Debug.WriteLine("API call successful.");
+
+                    var jsonString = response.Result.Content.ReadAsStringAsync().Result;
+                    var productList = JsonConvert.DeserializeObject<List<T>>(jsonString);
+
+                    return productList;
                 }
-            }
-            var response = client.GetAsync(uri); // Should move this to Task to be proper async
-            //response.Result.EnsureSuccessStatusCode();
-
-            if (!response.Result.IsSuccessStatusCode)
-            {
-                System.Diagnostics.Debug.WriteLine("API call failed.");
-                System.Diagnostics.Debug.WriteLine($"Error: {response.Result.Content.ReadAsStringAsync().Result}");
-
-                return new List<T>();
-            }
-            System.Diagnostics.Debug.WriteLine("API call successful.");
-
-            var jsonString = response.Result.Content.ReadAsStringAsync().Result;
-            var productList = JsonConvert.DeserializeObject<List<T>>(jsonString);
-
-            return productList;
+            }  
         }
     }
 }
